7772b729e67f079697ba6e4d2255aa90e64a48eb
Anwesh43 <anweshthecool0@gmail.com>
2018-02-06 07:50:59 +0530

    adding HLineView class

--- hlineview/src/main/java/ui/anwesome/com/hlineview/HLineView.kt
+++ hlineview/src/main/java/ui/anwesome/com/hlineview/HLineView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.hlineview
 /**
  * Created by anweshmishra on 06/02/18.
  */
+import android.content.Context
+import android.graphics.*
+import android.view.*
+class HLineView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
