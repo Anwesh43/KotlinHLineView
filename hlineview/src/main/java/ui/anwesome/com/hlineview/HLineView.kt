413494db014cff5f76daaf3382febd7d3775fd05
Anwesh43 <anweshthecool0@gmail.com>
2018-02-06 07:58:13 +0530

    adding HLineJoint

--- hlineview/src/main/java/ui/anwesome/com/hlineview/HLineView.kt
+++ hlineview/src/main/java/ui/anwesome/com/hlineview/HLineView.kt
@@ -19,4 +19,29 @@ class HLineView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class HLineJoint(var w:Float,var h:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(w/2-w/8,h/2)
+            canvas.rotate(-90f)
+            canvas.drawLine(0f,0f,w/8,0f,paint)
+            canvas.restore()
+            canvas.save()
+            canvas.translate(w/2-w/8,h/2-w/4)
+            canvas.rotate(180f)
+            canvas.drawLine(0f,0f,0f,-w/8,paint)
+            canvas.restore()
+            canvas.save()
+            canvas.translate(w/2-w/8,h/2)
+            canvas.rotate(90f)
+            canvas.drawLine(0f,0f,0f,-w/8,paint)
+            canvas.restore()
+        }
+        fun update(stopcb:(Float)->Unit) {
+
+        }
+        fun startUpdating(startcb:()->Unit) {
+
+        }
+    }
 }
